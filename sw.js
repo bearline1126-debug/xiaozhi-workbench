c23e6b6253d7ca22226fdcc8940cf566642f727a
小彘 <xiaozhi@local>
2026-08-12 11:51:33 +0800

    v22: 运动时长恢复+大入口改名补全+日记本双页内容+星愿瓶重做

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
-const CACHE = 'xiaozhi-workbench-v21';
+const CACHE = 'xiaozhi-workbench-v22';
 const ASSETS = ['./', './index.html', './manifest.json', './icon.png', './icon-192.png', './assets/welcome-default.jpg'];
-const BUILD = '2026-08-12-v21';
+const BUILD = '2026-08-12-v22';
 
 const DEFAULT_MANIFEST = {
   name: '小彘的工作台', short_name: '小彘',
