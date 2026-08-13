636f010341740fba1275f5f1febeb7bad2c09e0e
cheng <cheng@example.com>
2026-08-13 17:07:39 +0800

    v38: 首页拖动按zone分组+长按显示编辑控件(缩小/放大/折叠/删除) / pin改为置顶到更多 / 灵感满行+按钮换行 / 灵感·日写入今天的明日必做(不跳到今日必做) / 删MustEditor 3件限制+冗长hint / 视觉灵感按分类并排+只显示最新一张+点开看全部 / BUILD_VERSION=v38

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
-const CACHE = 'xiaozhi-workbench-v37';
+const CACHE = 'xiaozhi-workbench-v38';
 const ASSETS = ['./', './index.html', './manifest.json', './icon.png', './icon-192.png', './assets/welcome-default.jpg'];
-const BUILD = '2026-08-13-v37';
+const BUILD = '2026-08-13-v38';
 
 const DEFAULT_MANIFEST = {
   name: '小彘的工作台', short_name: '小彘',
