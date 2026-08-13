cde83622f8dade3eb49ea33dfe07b07413206c10
WorkBuddy <workbuddy@local>
2026-08-13 22:24:16 +0800

    v48: settings add restore-defaults (keep records) and clear-all (wipe cache+records) buttons

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
-const CACHE = 'xiaozhi-workbench-v47';
+const CACHE = 'xiaozhi-workbench-v48';
 const ASSETS = ['./', './index.html', './manifest.json', './icon.png', './icon-192.png', './assets/welcome-default.jpg'];
-const BUILD = '2026-08-13-v47';
+const BUILD = '2026-08-13-v48';
 
 const DEFAULT_MANIFEST = {
   name: '小彘的工作台', short_name: '小彘',
