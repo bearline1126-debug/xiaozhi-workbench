d886464683b72aca84686018dac3e68d9e32f584
WorkBuddy <workbuddy@local>
2026-08-13 21:42:41 +0800

    v47: revert icon to original landscape

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
-const CACHE = 'xiaozhi-workbench-v46';
+const CACHE = 'xiaozhi-workbench-v47';
 const ASSETS = ['./', './index.html', './manifest.json', './icon.png', './icon-192.png', './assets/welcome-default.jpg'];
-const BUILD = '2026-08-13-v46';
+const BUILD = '2026-08-13-v47';
 
 const DEFAULT_MANIFEST = {
   name: '小彘的工作台', short_name: '小彘',
