ebfc160cd0652cad0fb5a0cfda84f87110e31ae7
WorkBuddy <workbuddy@local>
2026-08-13 21:08:12 +0800

    v44: 追踪表习惯压缩(无desc文字+紧凑pill行)；欢迎页删壁纸+删开始按钮+3秒自动进入；桌面长按从340ms改为700ms防误移

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
-const CACHE = 'xiaozhi-workbench-v43';
+const CACHE = 'xiaozhi-workbench-v44';
 const ASSETS = ['./', './index.html', './manifest.json', './icon.png', './icon-192.png', './assets/welcome-default.jpg'];
-const BUILD = '2026-08-13-v43';
+const BUILD = '2026-08-13-v44';
 
 const DEFAULT_MANIFEST = {
   name: '小彘的工作台', short_name: '小彘',
