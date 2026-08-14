43e4809505f83916d5c2fc909cf6c2a8ce091905
WorkBuddy <workbuddy@local>
2026-08-14 11:57:14 +0800

    v53: 6项反馈（笔记卡片column布局/菜篮子显眼/桌面转盘可点/份子钱row均分/下拉项×删除/英语字号统一）

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
-const CACHE = 'xiaozhi-workbench-v52';
+const CACHE = 'xiaozhi-workbench-v53';
 const ASSETS = ['./', './index.html', './manifest.json', './icon.png', './icon-192.png', './assets/welcome-default.jpg'];
-const BUILD = '2026-08-14-v52';
+const BUILD = '2026-08-14-v53';
 
 const DEFAULT_MANIFEST = {
   name: '小彘的工作台', short_name: '小彘',
