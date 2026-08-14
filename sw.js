3076a19541b61ae1d152e5ac155ab6d8a6f26cc9
WorkBuddy <workbuddy@local>
2026-08-14 10:51:00 +0800

    v51: 6项反馈修复（按钮往左/复制真生效/转盘去前缀/份子钱人名下拉/桌面1×2/英语今日测试）

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
-const CACHE = 'xiaozhi-workbench-v50';
+const CACHE = 'xiaozhi-workbench-v51';
 const ASSETS = ['./', './index.html', './manifest.json', './icon.png', './icon-192.png', './assets/welcome-default.jpg'];
-const BUILD = '2026-08-14-v50';
+const BUILD = '2026-08-14-v51';
 
 const DEFAULT_MANIFEST = {
   name: '小彘的工作台', short_name: '小彘',
