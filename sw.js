87713131055344fb741f988e8bb2fd0bf066450d
cheng <cheng@local>
2026-08-15 09:27:32 +0800

    v63: 修全屏关闭跳桌面根因(App popstate不接管uiFs栈+null用hash兜底);菜篮子dishPhoto加multiple+add并行存多图+photoId数组+详情网格显图;经验摘录openDetail加📋复制全文符号按钮(放🖼旁)+搜索框放大按钮缩小

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
-const CACHE = 'xiaozhi-workbench-v62';
+const CACHE = 'xiaozhi-workbench-v63';
 const ASSETS = ['./', './index.html', './manifest.json', './icon.png', './icon-192.png', './assets/welcome-default.jpg'];
-const BUILD = '2026-08-14-v62';
+const BUILD = '2026-08-14-v63';
 
 const DEFAULT_MANIFEST = {
   name: '小彘的工作台', short_name: '小彘',
